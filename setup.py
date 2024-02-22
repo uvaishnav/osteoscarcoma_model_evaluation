385b5e6edc3fae4dabc3c52c029c700f5d42be97
uvaishnav <vuppalap@gitam.in>
2024-02-22 19:20:04 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+from typing import List
+
+hipen_e = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if(hipen_e in requirements):
+            requirements.remove(hipen_e)
+    
+    return requirements
+
+
+
+
+setup(
+    name = "osteoscarcoma_evaluation",
+    version = "0.0.1",
+    author = "Vaishnav, Agasthya, Lokesh, Aravind",
+    packages = find_packages(),
+    install_requires = get_requirements("requirements.txt")
+)
